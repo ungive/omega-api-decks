df745eb2f856bc98be8f705fd4db9350cbdf6147
vonas <24623262+vonas@users.noreply.github.com>
2020-07-27 15:38:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# deck-image
+
+Create an image of your Yu-Gi-Oh! deck by submitting a deck list in any of these formats:
+- A list (TODO: specify this more clearly)
+- YDK
+- YDKE
+- Omega Code
---
+++ web/create.php
@@ -0,0 +1,12 @@
+<?php
+
+function get_image($id) {
+    // ...
+    return null;
+}
+
+# - any non-extra-deck card goes to the main deck
+# - the first 15 extra deck cards go to the extra deck
+# - after the first extra deck card, any non-extra-deck card goes to the side deck
+
+phpinfo();
